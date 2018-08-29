591d1bfc9acc797854224cef4d92c7d479bd8d1f
Alba <sebasjosue84@gmail.com>
2018-08-28 19:55:32 -0600

    Abstract Factory Code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
+/**
+ * Instituto Tecnológico de Costa Rica
+ * Profesor: Isaac Ramírez
+ * Estudiantes: Sebastián Alba, Isaac Benavidez y Gabriel Brenes
+ * Ingeniería en computadores
+ * Patrones de diseño en C++
+
+ * Bibliografía y Webgrafía:
+ * 1.C++ Programming: Code patterns design - Wikibooks, open books for an open world. (2018).
+ * Retrieved from https://en.wikibooks.org/wiki/C%2B%2B_Programming/Code/Design_Patterns
+ * 2.Design Patterns (C++): Introduction - 2018. (2018). Retrieved from http://www.bogotobogo.com/
+ * DesignPatterns/introduction.php
+ */
 #include <string>
 #include <iostream>
 #include <memory>
 #include "Adapter.cpp"
 #include "Facade.cpp"
 #include "Observer.cpp"
+
 
 /** PATRON BUILDER
  * El patrón Builder se usa para separar la construcción de un objeto complejo de su representación para que el mismo proceso
@@ -123,6 +137,67 @@ public:
 private:
     SandwichBuilder* m_SandwichBuilder;
 };
+/**
+ * ABSTRACT FACTORY
+     * Abstract Factory: Crea una instancia de varias familias de clases.Es útil en una situación que requiere la creación de
+     * muchos tipos diferentes de objetos, todos derivados de un tipo base común.El método Factory define un método para crear
+     * los objetos, que las subclases pueden reemplazar para especificar el tipo derivado que se creará.
+     *
+     * Solución: definir una interfaz para crear un objeto, pero dejando que las subclases decidan qué clase se creará.
+     * El método Factory permite que una clase demore la creación de instancias a subclases.En el ejemplo siguiente,
+     * se utiliza un método Factory para crear objetos de ordenador portátil o de escritorio en tiempo de ejecución.
+     */
+
+class Pizza {
+public:
+    virtual int getPrice() const = 0;
+    virtual ~Pizza() {};  /*Sin esto,  no se llamará al destructor para las pizzas derivadas */
+};
+
+class SupremaPizza : public Pizza {
+public:
+    virtual int getPrice() const { return 850; };
+    virtual ~SupremaPizza() {};
+};
+
+class MantequillaPizza : public Pizza {
+public:
+    virtual int getPrice() const { return 1050; };
+    virtual ~MantequillaPizza() {};
+};
+
+class HawaianaPizza : public Pizza {
+public:
+    virtual int getPrice() const { return 1150; };
+    virtual ~HawaianaPizza() {};
+};
+
+class PizzaFactory {
+public:
+    enum TipodePizza {
+        Suprema,
+        Mantequilla,
+        Hawaiana
+    };
+
+    static unique_ptr<Pizza> createPizza(TipodePizza TipodePizza) {
+        switch (TipodePizza) {
+            case Suprema: return make_unique<SupremaPizza>();
+            case Mantequilla:      return make_unique<MantequillaPizza>();
+            case Hawaiana:    return make_unique<HawaianaPizza>();
+        }
+        throw "Tipo de pizza inválido.";
+    }
+};
+
+/*
+* Crear todas las pizzas e imprimir sus precios
+*/
+void pizza_information(PizzaFactory::TipodePizza TipodePizza)
+{
+    unique_ptr<Pizza> pizza = PizzaFactory::createPizza(TipodePizza);
+    cout << "El precio de la pizza " << TipodePizza << " es " << pizza->getPrice()  << " colónes. " << std::endl;
+}
 
 int main()
 {
@@ -189,4 +264,27 @@ int main()
 
     delete cliente1, cliente2, subway;
 
-}
+
+    /**
+     * Factory
+     */
+    cout << "\n\nAbstract Factory \n" << endl;
+    pizza_information(PizzaFactory::Suprema);
+    pizza_information(PizzaFactory::Mantequilla);
+    pizza_information(PizzaFactory::Hawaiana);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
